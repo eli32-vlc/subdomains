f21b643d1febfbbc7c38de07e4a4b464bf27f86c
Eason Li (zenith) <84105075+eli32-vlc@users.noreply.github.com>
2024-06-25 04:04:48 -0700

    Update and rename template.js to *.zenith.js

--- domains/proj.sbs/*.zenith.js
+++ domains/proj.sbs/*.zenith.js
@@ -2,18 +2,18 @@
 export default {
     owner: {
         // your github username
-        user: "proj-at",
+        user: "eli32-vlc",
         // your github email
-        email: "github@proj.at",
+        email: "lyu63651@gmail.com",
     },
     records: [
         // can include multiple records, if some of them conflict, may be overwritten
         {
             // type of DNS record
-            type: "CNAME",
-            record: "cname.vercel-dns.com.",
+            type: "NS",
+            record: ["ns53.cloudns.net", "ns54.cloudns.net"], // array of record values
             // using Cloudflare CDN
-            proxied: true,
+            proxied: false,
             // TTL, (s), must be between 60 and 86400
             ttl: 60,
         },
